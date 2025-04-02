2952e13dcfd836f258642d3081de5587aa136e71
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-02 18:19:22 +0800

    services: Pin system_server to top-app
    Change-Id: I2df4c94e1d958d059cee941ffa665bfab192addc

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -883,8 +883,9 @@ public final class SystemServer implements Dumpable {
             BinderInternal.setMaxThreads(sMaxBinderThreads);
 
             // Prepare the main looper thread (this thread).
-            android.os.Process.setThreadPriority(
-                    android.os.Process.THREAD_PRIORITY_FOREGROUND);
+            Process.setThreadPriority(
+                    Process.THREAD_PRIORITY_FOREGROUND);
+            Process.setThreadGroupAndCpuset(myPid(), Process.THREAD_GROUP_TOP_APP);
             android.os.Process.setCanSelfBackground(false);
             Looper.prepareMainLooper();
             Looper.getMainLooper().setSlowLogThresholdMs(
